3c0a1009a1274e1341d5dda9f121320706d44a96
Hanzala <siddiquehanzala@gmail.com>
2023-07-27 02:15:37 +0530

    second commit

---
+++ server.py
@@ -0,0 +1,47 @@
+import http.client
+import time
+import random
+
+class Proxy:
+    """This class creates a proxy server."""
+
+    def __init__(self):
+        """Initialize the proxy server."""
+        self.cache = {}  # A dictionary to store the responses from the remote server.
+        self.servers = ['localhost:80', 'localhost:443']  # A list of servers to proxy the request to.
+
+    def proxy(self, host, port, url):
+        """Proxy an HTTP request to the specified host and port."""
+        if url in self.cache:
+            """Check if the response is already in the cache."""
+            content = self.cache[url]
+        else:
+            """If not, proxy the request to the remote server."""
+            server = random.choice(self.servers)  # Randomly select a server to proxy the request to.
+            conn = http.client.HTTPConnection(server)  # Create a connection to the server.
+            request = conn.request('GET', url)  # Send a GET request to the server.
+            response = conn.getresponse()  # Get the response from the server.
+
+            if response.status == 200:
+                """If the response is successful, store the response in the cache and return it."""
+                content = response.read()
+                self.cache[url] = content
+
+            else:
+                """If the response is not successful, return None."""
+                content = None
+
+            conn.close()  # Close the connection to the server.
+
+        return content  # Return the content of the response.
+
+def main():
+    """The main function."""
+    proxy_server = Proxy()  # Create a proxy server.
+    url = 'http://localhost:8080/'  # The URL of the resource to request.
+    content = proxy_server.proxy('localhost', 8080, url)  # Proxy the request to the remote server.
+    print(content)  # Print the content of the response.
+
+if __name__ == '__main__':
+    """Run the main function."""
+    main()
